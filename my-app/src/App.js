717b04654aa20f88c0479707657edc4eb3f274ad
hsuanya129 <37055177+hsuanya129@users.noreply.github.com>
2019-07-15 16:19:38 +0800

    welcome page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,25 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  
+  constructor(props){
+    super(props);
+    this.state={
+      textValue:'DefaultValue'
+    }
+    this.getText = this.getText.bind(this);
+  
+  }
+
+  getText(event){
+    this.setState({
+      textValue:document.getElementById('textBox').value
+    });
+    console.log(this.state.textValue)
+    event.preventDefault();
+  }
+
+  render(){
+    
+    return(
+      <div>
+        <p id='welcome'>Welcome {this.state.textValue}</p>
+        <form onSubmit={this.getText}>
+          <input id='textBox' type='text' ></input>
+          <input type='submit' value='submit'></input>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;

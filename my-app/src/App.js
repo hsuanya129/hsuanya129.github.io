315e7d57eeabd25b0eb1ca604c7956826c70a206
hsuanya129 <37055177+hsuanya129@users.noreply.github.com>
2019-07-15 16:27:05 +0800

    Welcome Page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,15 +18,13 @@ class App extends React.Component{
     this.setState({
       textValue:document.getElementById('textBox').value
     });
-    console.log(this.state.textValue)
     event.preventDefault();
   }
 
   render(){
-    
     return(
       <div>
-        <p id='welcome'>Welcome {this.state.textValue}</p>
+        <p>Welcome {this.state.textValue}</p>
         <form onSubmit={this.getText}>
           <input id='textBox' type='text' ></input>
           <input type='submit' value='submit'></input>

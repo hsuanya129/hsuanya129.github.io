54535a5e693d65bed72f48c705b9a6cee5b67f52
hsuanya129 <37055177+hsuanya129@users.noreply.github.com>
2019-07-16 15:06:37 +0800

    Updates

---
+++ precache-manifest.1d296ce337eb9b940198415f635ddc23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6828da3108e90f5f33b34e0c413126c4",
+    "url": "/welcome_page/index.html"
+  },
+  {
+    "revision": "8c75a5963fe6c9df8a15",
+    "url": "/welcome_page/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/welcome_page/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "8c75a5963fe6c9df8a15",
+    "url": "/welcome_page/static/js/main.13a80418.chunk.js"
+  },
+  {
+    "revision": "145811b2a7c7aee228c9",
+    "url": "/welcome_page/static/js/runtime~main.9ad7c855.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/welcome_page/static/media/logo.5d5d9eef.svg"
+  }
+]);
